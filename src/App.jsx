20d9a0c2cad8cd8d2589d2dd1be6d647a2d2efb9
chandan632 <chandanorut737@gmail.com>
2020-08-28 21:53:44 +0530

    :)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <u>
+            <i>Welcome To The World Of React!!!</i>
+          </u>
+        </h1>
+      </div>
+    );
+  }
+}
